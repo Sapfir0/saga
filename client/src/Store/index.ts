dd6833bb0a8fa1aebe5a21ee1cdd427b42b85b25
Sapfir0 <sapfir999999@yandex.ru>
2020-07-06 13:16:32 +0400

    Забиндил верно классы к интерфейсам

--- client/src/Store/index.ts
+++ client/src/Store/index.ts
@@ -1,5 +1,6 @@
 import createSagaMiddleware from "redux-saga";
 import {applyMiddleware, createStore} from "redux";
+import "../typings/inversify.config"
 import {reducer} from "./reducers";
 import logger from "redux-logger";
 import rootSaga from "./Saga";
--- client/src/typings/inversify.config.ts
+++ client/src/typings/inversify.config.ts
@@ -5,12 +5,19 @@ import {TYPES} from "../services/typings/types";
 import ApiHelper from "../services/ApiHelper";
 import InteractionService from "../services/InteractionService";
 import {Saga} from "../Store/Saga";
-
+import {IApiHelper} from "../services/typings/IApiHelper";
+import {IInteractionService} from "../services/typings/IInteractionService";
+import {ISaga} from "../Store/typings/ISaga";
 
 const myContainer = new Container();
-myContainer.bind<ApiHelper>(TYPES.ApiHelper).to(InteractionService)
-myContainer.bind<Actions>(TYPES.Actions).to(Saga)
-myContainer.bind<InteractionService>(TYPES.InteractionService).to(Saga)
+
+myContainer.bind<IApiHelper>(TYPES.ApiHelper).to(ApiHelper)
+myContainer.bind<IInteractionService>(TYPES.InteractionService).to(InteractionService)
+myContainer.bind<ISaga>(TYPES.Saga).to(Saga)
+
+// myContainer.bind<ApiHelper>(TYPES.ApiHelper).to(InteractionService)
+// myContainer.bind<Actions>(TYPES.Actions).to(Saga)
+// myContainer.bind<InteractionService>(TYPES.InteractionService).to(Saga)
 
 
 export { myContainer }
