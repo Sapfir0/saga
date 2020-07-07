aeb758b6c016899c3752521ae4aed32523ea3dfd
Sapfir0 <sapfir999999@yandex.ru>
2020-07-07 11:11:45 +0400

    Добавил роуты для токенов

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import bodyParser from 'body-parser';
 import cors from 'cors';
-
+import jwt from "jwt"
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -18,6 +18,43 @@ app.get('/', function (req, res) {
 
 app.get('/getColors', function (req, res) {
     res.send(colors);
+});
+
+
+const validUsername = "admin"
+const validPassword = "1234"
+const secret = "secret key"
+
+app.post('/signIn',  urlencodedParser,  function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const user = {
+        username,
+        password,
+        'role': 'admin'
+    };
+    const token = jwt.sign(user, secret, {expiresIn: 300});
+    res.send({jwt: token, refreshToken: refreshToken})
+});
+
+app.post('/token', function (req, res, next) {
+    const username = req.body.username
+    const refreshToken = req.body.refreshToken
+    if((refreshToken in refreshTokens) && (refreshTokens[refreshToken] == username)) {
+        var user = {
+            'username': username,
+            'role': 'admin'
+        }
+        var token = jwt.sign(user, SECRET, { expiresIn: 300 })
+        res.json({token: 'JWT ' + token})
+    }
+    else {
+        res.send(401)
+    }
+})
+
+app.post('/register',  urlencodedParser,  function (req, res) {
+
 });
 
 app.post('/getColor',  urlencodedParser,  function (req, res) {
